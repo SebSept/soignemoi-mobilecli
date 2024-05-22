1421c617539fbcc5cc316fe0cd34af5ce13ea4f1
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-05-22 16:22:30 +0200

    url du cli en https

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,6 @@ class _MyWebViewController extends WebViewController {
   _MyWebViewController() {
     super.setJavaScriptMode(JavaScriptMode.unrestricted);
     super.setBackgroundColor(const Color(0x00000000));
-    super.loadRequest(Uri.parse('http://cli.ecf.seb7.fr/'));
+    super.loadRequest(Uri.parse('https://cli.ecf.seb7.fr/'));
   }
 }
